52ed6e96c635107fd5aa2b42297b1a776602fe52
Priya <pitlapriya4u@gmail.com>
2026-04-10 15:09:46 +0200

    React HelloWorld challenge done

---
+++ react-basics_hello-world/src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  // return <div>Say hello...</div>;
+ // return <h1>Hello world!</h1>
+ return <HelloWorld/>
+}
+
+function HelloWorld(){
+  return <h1>Hello world!</h1>
+}
