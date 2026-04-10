ed0be682e08ccd924f4874c80e1816255ec32d92
Priya <pitlapriya4u@gmail.com>
2026-04-10 15:20:02 +0200

    react hello-world article tassk is done

---
+++ react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorldArticle/>;
+}
+
+function HelloWorldArticle() {
+  return <article>
+    <h1>Hello World!</h1>
+    <p>This is my First React Hello World component!!</p>
+    </article>
+}
