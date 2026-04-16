f7f9ba889b4f2f92130977c58152302175fa64fd
Priya <pitlapriya4u@gmail.com>
2026-04-16 16:14:05 +0200

    react state_tags task done

---
+++ react-state3/react-state-3_tags/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./styles.css";
+import Form from "./components/Form";
+import List from "./components/List";
+
+export default function App() {
+  const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
+  
+  function handleAddTag(newTag){
+      setTags((prevTags) => [...prevTags,newTag]);
+  }
+
+  function handleDeleteTag(tagToDelete){
+      setTags((prevTags) => 
+         prevTags.filter((tag) => tag !== tagToDelete)
+    );
+  }
+
+  return (
+    <main className="app">
+      <Form onAddTag = {handleAddTag}/>
+      <List  onDeleteTag = {handleDeleteTag}  tags={tags} />
+    </main>
+  );
+}
