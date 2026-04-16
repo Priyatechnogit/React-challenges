21ecc91b21d214349c16034c24b5b2db90e67db3
Priya <pitlapriya4u@gmail.com>
2026-04-16 12:01:19 +0200

    table reservation task done with state

---
+++ React-state2/react-state-2_table-reservation/src/App.jsx
@@ -0,0 +1,26 @@
+import Counter from "./components/Counter";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+   const [people, setPeople] = useState(2);
+
+   const onAddPeople = () => {
+    setPeople(people + 1);
+   };
+
+    const onRemovePeople = () => {
+    setPeople(people - 1);
+   };
+
+  return (
+    <div className="container">
+      <h1>Place a Table Reservation</h1>
+      <Counter 
+      onAddPeople={onAddPeople}
+      onRemovePeople={onRemovePeople}/>
+
+      <p>You are going to reserve a table for {people} people.</p>
+    </div>
+  );
+}
